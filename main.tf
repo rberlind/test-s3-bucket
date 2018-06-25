b89f74f01cd44a30ec1ecf8352d01e5bff3ffa27
Test User <eng-serv+bbstestuser@hashicorp.com>
2018-06-25 10:59:31 -0400

    added variables and provider

--- main.tf
+++ main.tf
@@ -1,10 +1,27 @@
+terraform {
+  required_version = ">= 0.11.0"
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  default = "us-east-1"
+}
+
+variable "bucket_name" {
+   description = "Name of the bucket to create"
+}
+
 variable "bucket_acl" {
    description = "ACL for S3 bucket: private, public-read, public-read-write, etc"
    default = "private"
 }
 
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
 resource "aws_s3_bucket" "bucket" {
-  bucket = "roger-tf-test-bucket"
+  bucket = "${var.bucket_name}"
   acl    = "${var.bucket_acl}" 
 
   tags {
