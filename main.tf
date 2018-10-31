eeadbd3957a12dac1773eae823267b8e1ef42ba2
Roger Berlind <rberlind@optonline.net>
2018-10-30 20:21:57 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,8 +28,8 @@ resource "aws_s3_bucket" "bucket" {
     rule {
       apply_server_side_encryption_by_default {
         kms_master_key_id = "arn:aws:kms:us-east-1:753646501470:key/00c892e8-40c4-4048-a650-0f755876503d"
-        #sse_algorithm     = "aws:kms"
-        sse_algorithm     = "AES256"
+        sse_algorithm     = "aws:kms"
+        #sse_algorithm     = "AES256"
       }
     }
   }
