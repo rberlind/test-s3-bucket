c00c82ccfb0dacd8bdbf07b0f89deb71cf1e8b84
Roger Berlind <roger@hashicorp.com>
2019-03-08 00:49:56 -0500

    use kms for sse_algorithm

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "aws_s3_bucket" "bucket" {
     rule {
       apply_server_side_encryption_by_default {
         kms_master_key_id = "arn:aws:kms:us-east-1:753646501470:key/00c892e8-40c4-4048-a650-0f755876503d"
-        sse_algorithm     = "AES256"
+        sse_algorithm     = "aws:kms"
       }
     }
   }
