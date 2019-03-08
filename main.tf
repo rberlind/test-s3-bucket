0eb467aeca695ed1cbd75e7936a5440324f0d239
Roger Berlind <roger@hashicorp.com>
2019-03-08 00:42:30 -0500

    remove versioning

--- main.tf
+++ main.tf
@@ -33,10 +33,6 @@ resource "aws_s3_bucket" "bucket" {
     }
   }
   
-  versioning {
-    enabled = true
-  }
-  
   tags {
     Name        = "Roger Test Bucket"
     Owner = "roger@hashicorp.com"
